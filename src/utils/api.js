7e0b0a42516170b0ed422a231de8f81263192c17
Davi <davilinfo@gmail.com>
2022-04-27 20:29:08 -0300

    css and new fee for archive and file transactions

--- src/utils/api.js
+++ src/utils/api.js
@@ -32,37 +32,37 @@ let clientCache;
 
 const networkIdentifier = "a5f4ae7dd207c8d9767c10ec17544ec46eacd9b351ecbdda5c6e97a0dfc5acd2";
 
-async function getClient () { 
-  if (!clientCache) {         
+async function getClient () {
+  if (!clientCache) {
     clientCache = await apiClient.createWSClient(wsNode);
     console.log("created ws client");
     return clientCache;
   }
 
   console.log("memory ws client");
-           
+
   return clientCache;
 };
 
 async function getAccountFromAddress (address)  {
-  const client = await getClient();  
+  const client = await getClient();
   const account = await client.invoke('app:getAccount', {
-      address,      
+      address,
   });
   var decoded = await client.account.decode(Buffer.from(account, 'hex'));
-  
+
   return await client.account.toJSON(decoded);
 };
 
-async function getAccountNonce (account) {    
+async function getAccountNonce (account) {
   return Number(account.sequence.nonce);
 };
 
 async function getTransactionById (id)  {
-  const client = await getClient();  
+  const client = await getClient();
   const transaction = await client.invoke('app:getTransactionByID', {id: id});
   const decoded = await client.transaction.decode(Buffer.from(transaction, 'hex'))
-  
+
   return await client.transaction.toJSON(decoded);
 };
 
@@ -72,61 +72,61 @@ const getTransactionsByPersistedIds = async(options) => {
     var reqOptions = {
       method: "POST",
       mode: "cors",
-      headers: { "Content-Type": "application/json" }      
+      headers: { "Content-Type": "application/json" }
     };
-    var req = await fetch (url, reqOptions);   
+    var req = await fetch (url, reqOptions);
     var result = await req.json();
     var file = JSON.parse(result);
-    
+
     var ids = [];
 
-    file.transactions.forEach(element => {      
+    file.transactions.forEach(element => {
       ids.push(element.transactionId);
-    });    
+    });
 
   return await getTransactionsByIds(ids, options);
 }
 
 const getTransactionsByIds = async(ids, options) => {
   const client = await getClient();
-    
+
   var transactions = await client.invoke('app:getTransactionsByIDs', {"ids": ids});
-  
+
   var decodedTransactions = [];
 
   for (var index=transactions.length-1;index >= 0; index--)
-  {    
+  {
     var id = transactions[index];
     const transactionObject = await client.transaction.decode(Buffer.from(id, 'hex'));
-    const transactionJSON = await client.transaction.toJSON(transactionObject);    
-    
+    const transactionJSON = await client.transaction.toJSON(transactionObject);
+
     console.log(transactionObject);
-    
+
     if (transactionJSON.assetID !== options.type){
       continue;
-    }    
-        
-    const transactionSenderPublicKey = cryptography.getBase32AddressFromPublicKey(transactionObject.senderPublicKey);    
+    }
+
+    const transactionSenderPublicKey = cryptography.getBase32AddressFromPublicKey(transactionObject.senderPublicKey);
     if (transactionSenderPublicKey !== options.senderId){
       continue;
     }
     console.log(transactionJSON);
     decodedTransactions.push(transactionJSON);
-  }  
+  }
 
   return decodedTransactions;
 }
 
-const getBlockByHeightWithFilter = async(options, index, resultList) => {  
+const getBlockByHeightWithFilter = async(options, index, resultList) => {
   const client = await getClient();
-  
+
   var previousBlockId;
   var current = await client.invoke('app:getBlockByID', {"id": index});
   const blockObject = await client.block.decode(Buffer.from(current, 'hex'));
   const blockJSON = await client.block.toJSON(blockObject);
-  
-  previousBlockId = blockJSON.header.previousBlockID;  
-  
+
+  previousBlockId = blockJSON.header.previousBlockID;
+
   if (blockJSON.payload && blockJSON.payload.length > 0) {
     console.log(blockJSON);
     console.log("block height with transaction(s)", index);
@@ -142,61 +142,61 @@ const getBlockByHeightWithFilter = async(options, index, resultList) => {
         }
         console.log("including transaction in result")
         resultList.push(blockJSON.payload[payloadIndex]);
-      }            
+      }
     }
   }
-  
+
   if (blockJSON.header.height > 13800){
     console.log("recursive verification", blockJSON.header.height);
     await getBlockByHeightWithFilter(options, previousBlockId, resultList);
   }
-  
+
   return resultList;
 }
 
-const getBlocks = async(options) => {  
+const getBlocks = async(options) => {
   var resultList = [];
   const client = await getClient();
   const node = await client.invoke('app:getNodeInfo', {});
   var index = node.lastBlockID;
-  
-  const result = await getBlockByHeightWithFilter(options, index, resultList); 
+
+  const result = await getBlockByHeightWithFilter(options, index, resultList);
 
   return result;
 }
 
-const getTransactionFromPoolFeePerAccount = async (publicKey, nonce) => {        
-  const client = await getClient();    
+const getTransactionFromPoolFeePerAccount = async (publicKey, nonce) => {
+  const client = await getClient();
   const transactions = await client.invoke('app:getTransactionsFromPool', {});
 
-  var pubKeyHex = cryptography.bufferToHex(publicKey);   
-  console.log("nonce", nonce); 
+  var pubKeyHex = cryptography.bufferToHex(publicKey);
+  console.log("nonce", nonce);
   console.log("account pubKey",pubKeyHex);
 
   for (var index=0;index < transactions.length;index++)
-  {                        
-      console.log("transactions in pool", transactions.length); 
+  {
+      console.log("transactions in pool", transactions.length);
 
       var transaction = transactions[index];
       var decoded = await client.transaction.decode(Buffer.from(transaction, 'hex'));
-      var transactionJSON = await client.transaction.toJSON(decoded);                
-      
-      console.log("transaction from pool", transactionJSON);      
+      var transactionJSON = await client.transaction.toJSON(decoded);
+
+      console.log("transaction from pool", transactionJSON);
 
       if (transactionJSON.senderPublicKey === pubKeyHex){
         if (transactionJSON.nonce >= nonce){
           console.log("fee", transactionJSON.fee);
           return transactionJSON.fee;
         }
-      }      
+      }
   };
   return 0;
 }
 
 const sendTransaction = async(transaction) => {
-  const client = await getClient();        
+  const client = await getClient();
   const result = await client.transaction.send(transaction);
-  
+
   return result;
 }
 
@@ -206,14 +206,14 @@ async function archiveText (form) {
     text: form.text
   }).length;
   const passphrase = form.passphrase ? form.passphrase : fundingPassphrase;
-  
-  const sender = cryptography.getAddressAndPublicKeyFromPassphrase(passphrase);  
+
+  const sender = cryptography.getAddressAndPublicKeyFromPassphrase(passphrase);
 
   var account = await getAccountFromAddress(sender.address);
   var accountNonce = await getAccountNonce(account);
 
   var poolFee = await getTransactionFromPoolFeePerAccount(sender.publicKey, accountNonce);
-  
+
   try{
   const tx = transactions.signTransaction(
     archiveTextSchema,
@@ -221,7 +221,7 @@ async function archiveText (form) {
         moduleID: 5000,
         assetID: 101,
         nonce: Helper(accountNonce),
-        fee: poolFee > 0 ? Helper(poolFee + 10) : Helper(formLength * 10000 * 1024 / 1000 * 10),
+        fee: poolFee > 0 ? Helper(poolFee + 10) : Helper(formLength * 10),
         senderPublicKey: sender.publicKey,
         asset: {
             data: JSON.stringify({
@@ -233,9 +233,9 @@ async function archiveText (form) {
         },
     },
     Buffer.from(networkIdentifier, "hex"),
-    passphrase);    
-        
-    var result = await sendTransaction(tx);    
+    passphrase);
+
+    var result = await sendTransaction(tx);
 
     var url= apiHelper + "/push";
     var options = {
@@ -244,19 +244,19 @@ async function archiveText (form) {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(result)
     };
-    var req = await fetch (url, options);    
+    var req = await fetch (url, options);
 
     console.log("request persist", req.status);
-    
-    return result;  
-    
+
+    return result;
+
   }catch(e){
     console.log("error signing transaction", e);
-  }   
+  }
 };
 
 const archiveFile = async (form) => {
-  
+
   console.log("archiveFile");
   const passphrase = form.passphrase ? form.passphrase : fundingPassphrase;
   const sender = cryptography.getAddressAndPublicKeyFromPassphrase(passphrase);
@@ -285,7 +285,7 @@ const archiveFile = async (form) => {
         moduleID: 5000,
         assetID: 102,
         nonce: Helper(accountNonce),
-        fee: poolFee > 0 ? Helper(poolFee + 10) : Helper(formLength * 10000 * 1024 / 1000 * 10),
+        fee: poolFee > 0 ? Helper(poolFee + 10) : Helper(formLength * 10),
         senderPublicKey: sender.publicKey,
         asset: {
             data: JSON.stringify({
@@ -297,11 +297,11 @@ const archiveFile = async (form) => {
         },
     },
     Buffer.from(networkIdentifier, "hex"),
-    passphrase);    
+    passphrase);
 
     try{
     var result = await sendTransaction(tx);
-    
+
     var url= apiHelper + "/push";
     var options = {
       method: "POST",
@@ -309,27 +309,27 @@ const archiveFile = async (form) => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(result)
     };
-    var req = await fetch (url, options);    
+    var req = await fetch (url, options);
 
     console.log("request persist", req.status);
-  
+
   }catch(e){
     console.log("error signing transaction", e);
   }
 
-  return result;   
+  return result;
 };
 
 export const processSubmission = async (form) => {
-  
-  if (form.type === "text") {    
+
+  if (form.type === "text") {
     return await archiveText(form);
   } else if (form.type === "file") {
     return await archiveFile(form);
   }
 };
 
-export const getTransactions = options => getTransactionsByPersistedIds(options).then(function(response){  
+export const getTransactions = options => getTransactionsByPersistedIds(options).then(function(response){
   return response;
 }).catch(function(e){
   console.log("error getTransactions", e);
